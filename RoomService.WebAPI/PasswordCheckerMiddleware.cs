ff5e72f9b0e3b584529f6893b1419f6cfd5883b4
Git Server Admin <admin@git-server>
2022-03-29 04:45:43 +0000

    Add initial repository

---
+++ RoomService.WebAPI/PasswordCheckerMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace RoomService.WebAPI
+{
+    public class PasswordCheckerMiddleware
+    {
+       
+        public PasswordCheckerMiddleware(RequestDelegate next)
+        {
+
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+        }
+    }
+}
