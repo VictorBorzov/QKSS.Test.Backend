499b92dd43526f7676e207935c45a9143c0b15dd
Victor Borzov <borzov@ap-team.ru>
2022-03-30 19:21:16 +0300

    Added Middleware with password header check (it extracts a password from env variable or use a default value). Added Docker support.

--- RoomService.WebAPI/PasswordCheckerMiddleware.cs
+++ RoomService.WebAPI/PasswordCheckerMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
@@ -6,15 +7,28 @@ namespace RoomService.WebAPI
 {
     public class PasswordCheckerMiddleware
     {
+        private readonly RequestDelegate _next;
        
         public PasswordCheckerMiddleware(RequestDelegate next)
         {
-
+            _next = next;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
-
+            const string passwordHeader = "passwordKey";
+            const string defaultPassword = "passwordKey123456789000";
+            var expectedPassword = Environment.GetEnvironmentVariable(passwordHeader) ?? defaultPassword;
+            context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+            if (context.Request.Headers.ContainsKey(passwordHeader))
+            {
+                var passed = string.Equals(context.Request.Headers[passwordHeader], expectedPassword, StringComparison.InvariantCulture);
+                if (passed)
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    await _next.Invoke(context);
+                }
+            }
         }
     }
 }
